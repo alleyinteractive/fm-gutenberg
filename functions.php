77ce02afff7c05fcb8e1a0938be42d0d71a944f8
Erick Hitter <git-contrib@ethitter.com>
2019-01-25 15:14:32 -0800

    Add missing namespace

--- functions.php
+++ functions.php
@@ -5,6 +5,8 @@
  * @package WP_Starter_Plugin
  */
 
+namespace WP_Starter_Plugin;
+
 // Content types and taxonomies should be included below. In order to scaffold
 // them, leave the Begin and End comments in place.
 /* Begin Data Structures */
