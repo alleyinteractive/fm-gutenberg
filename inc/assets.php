195b625464abaa25435d988818d492393e693328
jomurgel <jo@alley.co>
2019-09-11 12:12:42 -0600

    removes use of admin_enqueue_scripts and moves contents to enqueue_block_editor_assets call

--- inc/assets.php
+++ inc/assets.php
@@ -9,27 +9,9 @@ namespace WP_Starter_Plugin;
 
 // Register action and filter hooks.
 add_action(
-	'admin_enqueue_scripts',
-	__NAMESPACE__ . '\action_admin_enqueue_scripts'
-);
-add_action(
 	'enqueue_block_editor_assets',
 	__NAMESPACE__ . '\action_enqueue_block_editor_assets'
 );
-
-/**
- * Enqueue general-purpose scripts in the admin.
- */
-function action_admin_enqueue_scripts() {
-	wp_enqueue_script(
-		'wp-starter-plugin-plugin-sidebar',
-		plugins_url( 'build/pluginSidebar.js', __DIR__ ),
-		[ 'wp-i18n', 'wp-edit-post' ],
-		'1.0.0',
-		true
-	);
-	inline_locale_data( 'wp-starter-plugin-plugin-sidebar' );
-}
 
 /**
  * A callback for the enqueue_block_editor_assets action hook.
@@ -39,6 +21,15 @@ function action_enqueue_block_editor_assets() {
 
 	// Only enqueue the script to register the Sample Block if supported.
 	if ( 'post' === $post_type ) {
+		wp_enqueue_script(
+			'wp-starter-plugin-plugin-sidebar',
+			plugins_url( 'build/pluginSidebar.js', __DIR__ ),
+			[ 'wp-i18n', 'wp-edit-post' ],
+			'1.0.0',
+			true
+		);
+		inline_locale_data( 'wp-starter-plugin-plugin-sidebar' );
+
 		wp_enqueue_script(
 			'wp-starter-plugin-block-sample-block',
 			plugins_url( 'build/blockSampleBlock.js', __DIR__ ),
