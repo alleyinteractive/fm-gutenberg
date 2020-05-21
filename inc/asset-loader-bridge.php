33f1e021e1f9107686a6ad513d2de531c354c130
kevinfodness <kevin@kevinfodness.com>
2020-05-21 10:41:41 -0400

    Reset scaffolded files, add asset loader bridge

---
+++ inc/asset-loader-bridge.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * A bridge between the namespaced function for getting assets and the
+ * scaffolded block files. Can be removed once namespacing is fully
+ * supported by the scaffolder in a way that won't break integrations
+ * with existing sites.
+ *
+ * This file MUST NOT have a namespace defined in order to work!
+ *
+ * @package WP_Starter_Plugin
+ */
+
+/**
+ * A helper function to bridge the gap between the non-namespaced scaffolded
+ * block files and the namespaced block loader in the plugin.
+ *
+ * @param string  $asset_path Entry point and asset type separated by a '.'.
+ * @param boolean $dir_path   Whether to return the directory path or the plugin URL path.
+ *
+ * @return string The asset version.
+ */
+function get_versioned_asset_path( $asset_path, $dir_path = false ) {
+	return WP_Starter_Plugin\get_versioned_asset_path( $asset_path, $dir_path );
+}
+
+/**
+ * A helper function to bridge the gap between the non-namespaced scaffolded
+ * block files and the namespaced inline locale function in the plugin.
+ *
+ * @param string $to_handle The script handle to attach the inline script to.
+ */
+function inline_locale_data( $to_handle ) {
+	WP_Starter_Plugin\inline_locale_data( $to_handle );
+}
--- index.php
+++ index.php
@@ -15,6 +15,7 @@ namespace WP_Starter_Plugin;
 
 // Include functions for working with assets (primarily JavaScript).
 require_once __DIR__ . '/inc/assets.php';
+require_once __DIR__ . '/inc/asset-loader-bridge.php';
 
 // Include functions for working with meta.
 require_once __DIR__ . '/inc/meta.php';
