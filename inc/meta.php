5386329bd5e9bebbe595c10ff3ff96b77874bf82
Jake Foster <jake@alley.co>
2021-01-18 13:06:17 -0500

    add docbloc

--- inc/meta.php
+++ inc/meta.php
@@ -41,7 +41,34 @@ function register_meta_helper(
 		);
 	}
 
-	// Merge provided arguments with defaults.
+	/**
+	 * Merge provided arguments with defaults.
+	 * Filters register_*_meta() args.
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/register_meta/
+	 *
+	 * @param array $args {
+	 *     Array of args to be passed to register_*_meta() functions.
+	 *
+	 *     @type string     $object_subtype    A subtype; e.g. if the object type is "post", the post type. If left empty,
+	 * 										   the meta key will be registered on the entire object type. Default empty.
+	 *     @type string     $type              The type of data associated with this meta key. Valid values are
+	 * 										   'string', 'boolean', 'integer', 'number', 'array', and 'object'.
+	 *     @type string     $description       A description of the data attached to this meta key.
+	 *     @type bool       $single	           Whether the meta key has one value per object, or an array of values per object.
+	 * 	   @type mixed      $default           The default value returned from get_metadata() if no value has been set yet.
+	 *                                         When using a non-single meta key, the default value is for the first entry. In other words,
+	 * 									       when calling get_metadata() with $single set to false, the default value given here will be wrapped in an array.
+	 *     @type callable   $sanitize_callback A function or method to call when sanitizing $meta_key data.
+	 * 	   @type callable   $auth_callback     Optional. A function or method to call when performing edit_post_meta,
+	 *                                         add_post_meta, and delete_post_meta capability checks.
+	 *     @type bool|array $show_in_rest      Whether data associated with this meta key can be considered public and should be
+	 *                                         accessible via the REST API. A custom post type must also declare support
+	 *                                         for custom fields for registered meta to be accessible via REST. When registering
+	 *                                         complex meta values this argument may optionally be an array with 'schema'
+	 *                                         or 'prepare_callback' keys instead of a boolean.
+	 * }
+	 */
 	$args = apply_filters( 'ai_register_meta_helper_args', wp_parse_args(
 		$args,
 		[
