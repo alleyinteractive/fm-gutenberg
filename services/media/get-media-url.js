c501a21d4997f815de93f706348744f365ff0697
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2021-03-22 06:48:31 -0400

    Update services/media/get-media-url.js
    Co-authored-by: Renato Alves <renato@alley.co>

--- services/media/get-media-url.js
+++ services/media/get-media-url.js
@@ -3,7 +3,7 @@
  * URL for the media item at the requested size if it exists, or the full
  * size if it does not. Returns an empty string if unable to find either.
  * @param {object} media - A media object returned by the WordPress API.
- * @param {string} size - Size to request.
+ * @param {string} size - Media size to request. Default: full
  * @returns {string} - The URL to the asset, or an empty string on failure.
  */
 const getMediaUrl = (media, size = 'full') => {
