5cfe28c031b7c3e6004f4c949232753ed6b8b3a3
jomurgel <jomurgel@users.noreply.github.com>
2021-04-01 11:09:49 -0600

    updates image thumbnail function/test

--- services/media/get-image-thumbnail.js
+++ services/media/get-image-thumbnail.js
@@ -3,19 +3,56 @@
  * thumbnail image for the media item if it exists, or the full size image
  * if it does not. Returns an empty string if unable to find either.
  * @param {object} media - A media object returned by the WordPress API.
+ * @param {string} size - Size to request if requested.
  * @returns {string} - The URL to the image, or an empty string on failure.
  */
-export default function getImageThumbnail(media) {
-  const {
-    media_details: {
+export default function getImageThumbnail(media, size = 'post-thumbnail') {
+  let mediaType = 'image';
+
+  if (media && (undefined !== media.media_type || undefined !== media.type)) {
+    mediaType = media.media_type || media.type;
+  }
+
+  if (mediaType === 'image') {
+    // If we're uploading, we'll get the media_details array.
+    if (media.media_details) {
+      const {
+        media_details: {
+          sizes: {
+            [size]: {
+              source_url: thumbnailUrl = '',
+            } = {},
+            thumbnail: {
+              source_url: defaultUrl = '',
+            } = {},
+          } = {},
+        } = {},
+      } = media;
+
+      return thumbnailUrl || defaultUrl;
+    }
+
+    const {
       sizes: {
-        'post-thumbnail': {
-          source_url: thumbnailUrl = '',
+        [size]: {
+          url: thumbnailUrl = '',
+        } = {},
+        thumbnail: {
+          url: defaultUrl = '',
         } = {},
       } = {},
-    } = {},
-    source_url: fullsizeUrl = '',
-  } = media;
+    } = media;
 
-  return thumbnailUrl || fullsizeUrl;
+    return thumbnailUrl || defaultUrl;
+  }
+
+  if (mediaType === 'file') {
+    const {
+      url: fileUrl = '',
+    } = media;
+
+    return fileUrl;
+  }
+
+  return '';
 }
--- services/media/get-image-thumbnail.spec.js
+++ services/media/get-image-thumbnail.spec.js
@@ -3,23 +3,55 @@
 import getImageThumbnail from './get-image-thumbnail';
 
 describe('getImageThumbnail', () => {
-  it('Should properly get an image thumbnail from a media object.', () => {
-    expect(getImageThumbnail({
-      media_details: {
-        sizes: {
-          'post-thumbnail': {
-            source_url: '/source/url/1',
+  if (getImageThumbnail({ media_details: {} })) {
+    it('Should properly get an image thumbnail from a media object.', () => {
+      expect(getImageThumbnail({
+        media_type: 'image',
+        media_details: {
+          sizes: {
+            thumbnail: {
+              source_url: '/source/url/1',
+            },
           },
         },
+        source_url: '/source/url/2',
+      })).toEqual('/source/url/1');
+    });
+
+    it('Should fall back to full size image if no thumbnail is present.', () => {
+      expect(getImageThumbnail({
+        media_type: 'image',
+        source_url: '/source/url/2',
+      })).toEqual('/source/url/2');
+    });
+  }
+
+  it('Should properly get an image thumbnail from a media object.', () => {
+    expect(getImageThumbnail({
+      type: 'image',
+      sizes: {
+        full: {
+          url: '/url/1',
+        },
+        thumbnail: {
+          url: '/url/2',
+        },
       },
-      source_url: '/source/url/2',
-    })).toEqual('/source/url/1');
+    })).toEqual('/url/2');
   });
 
-  it('Should fall back to full size image if no thumbnail is present.', () => {
+  it('Should properly get an image thumbnail from a media object.', () => {
     expect(getImageThumbnail({
-      source_url: '/source/url/2',
-    })).toEqual('/source/url/2');
+      media_type: 'file',
+      url: '/url/2',
+    })).toEqual('/url/2');
+  });
+
+  it('Should properly get an image thumbnail from a media object.', () => {
+    expect(getImageThumbnail({
+      type: 'file',
+      url: '/url/2',
+    })).toEqual('/url/2');
   });
 
   it('Should return an empty string if there is no image URL present.', () => {
