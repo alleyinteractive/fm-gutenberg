eb37454b182f1d8dbc670da4125949aa96776e88
kevinfodness <2650828+kevinfodness@users.noreply.github.com>
2021-07-23 07:38:13 -0400

    Upgrade dependencies, simplify configs

--- webpack.config.js
+++ webpack.config.js
@@ -6,73 +6,92 @@ const StatsPlugin = require('webpack-stats-plugin').StatsWriterPlugin;
 const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 const createWriteWpAssetManifest = require('./webpack/wpAssets');
 
-module.exports = (env, argv) => {
-  const { mode } = argv;
+module.exports = (env, { mode }) => ({
+  /*
+   * See https://webpack.js.org/configuration/devtool/ for an explanation of how
+   * to configure this directive. We are using the recommended options for
+   * production and development mode that produce high quality source maps.
+   * However, the performance of these options is not stellar, so if you
+   * notice that the build performance in your project is suffering to an
+   * unacceptable degree, you can choose different options from the link above.
+   */
+  devtool: mode === 'production'
+    ? 'source-map'
+    : 'eval-source-map',
 
-  return {
-    devtool: mode === 'production'
-      ? 'source-map'
-      : 'cheap-module-eval-source-map',
-    entry: glob
-      .sync('./blocks/**/index.js*')
-      .reduce((acc, item) => {
-        const entry = item
-          .replace('./blocks/', '')
-          .replace('/index.jsx', '')
-          .replace('/index.js', '');
-        acc[entry] = item;
-        return acc;
-      }, {
-        slotfills: './slotfills/index.js',
-      }),
-    module: {
-      rules: [
-        {
-          exclude: /node_modules/,
-          test: /.jsx?$/,
-          use: [
-            'babel-loader',
-            'eslint-loader',
-          ],
-        },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          loaders: [
-            'style-loader',
-            'css-loader',
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [autoprefixer()],
-              },
+  // Dynamically produce entries from the slotfills index file and all blocks.
+  entry: glob
+    .sync('./blocks/**/index.js*')
+    .reduce((acc, item) => {
+      const entry = item
+        .replace('./blocks/', '')
+        .replace('/index.jsx', '')
+        .replace('/index.js', '');
+      acc[entry] = item;
+      return acc;
+    }, {
+      slotfills: './slotfills/index.js',
+    }),
+
+  // Configure loaders based on extension.
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /.jsx?$/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer()],
             },
-            'resolve-url-loader',
-            'sass-loader',
-          ],
-        },
-      ],
-    },
-    output: {
-      filename: mode === 'production'
-        ? '[name].bundle.min.js'
-        : '[name].js',
-      path: path.join(__dirname, 'build'),
-    },
-    plugins: [
-      new DependencyExtractionWebpackPlugin(),
-      new StatsPlugin({
-        transform: createWriteWpAssetManifest,
-        fields: ['assetsByChunkName', 'hash'],
-        filename: 'assetMap.json',
-      }),
-      ...(mode === 'production'
-        ? [
-          new CleanWebpackPlugin(),
-        ] : []
-      ),
+          },
+          'resolve-url-loader',
+          'sass-loader',
+        ],
+      },
     ],
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-  };
-};
+  },
+
+  // Use different filenames for production and development builds for clarity.
+  output: {
+    filename: mode === 'production'
+      ? '[name].bundle.min.js'
+      : '[name].js',
+    path: path.join(__dirname, 'build'),
+  },
+
+  // Configure plugins.
+  plugins: [
+    // This maps references to @wordpress/{package-name} to the wp object.
+    new DependencyExtractionWebpackPlugin({ useDefaults: true }),
+
+    // This creates our assetMap.json file to get build hashes for cache busting.
+    new StatsPlugin({
+      transform: createWriteWpAssetManifest,
+      fields: ['assetsByChunkName', 'hash'],
+      filename: 'assetMap.json',
+    }),
+
+    // For production builds only, delete the contents of the build folder before building.
+    ...(mode === 'production'
+      ? [
+        new CleanWebpackPlugin(),
+      ] : []
+    ),
+  ],
+
+  // Tell webpack that we are using both .js and .jsx extensions.
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+});
