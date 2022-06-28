54daa1dfc1ab7877a0c5b53fdfbcc11cbd31903a
Greg Marshall <greg@alley.co>
2022-06-28 16:16:22 -0500

    fixing some sidebar styling

--- slotfills/sections/meta-box.jsx
+++ slotfills/sections/meta-box.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
-import { PanelBody } from '@wordpress/components';
+import { PanelRow } from '@wordpress/components';
 import usePostMetaValue from '@/hooks/use-post-meta-value';
 
 // Components.
@@ -18,19 +18,17 @@ const MetaBox = ({
     name={context.name}
     title={title}
   >
-    <PanelBody>
-      {context.field_class === 'group' ? (
-        <Group
-          field={context}
-          valueHook={usePostMetaValue}
-        />
-      ) : (
-        <FieldRouter
-          field={context}
-          valueHook={usePostMetaValue}
-        />
-      )}
-    </PanelBody>
+    {context.field_class === 'group' ? (
+      <Group
+        field={context}
+        valueHook={usePostMetaValue}
+      />
+    ) : (
+      <FieldRouter
+        field={context}
+        valueHook={usePostMetaValue}
+      />
+    )}
   </PluginDocumentSettingPanel>
 );
 
