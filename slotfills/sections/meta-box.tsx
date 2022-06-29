65d1ddd0462518228d07a7aa8b7a66b92fab1d02
Greg Marshall <greg@alley.co>
2022-06-29 12:19:36 -0500

    switch to interface

--- slotfills/sections/meta-box.tsx
+++ slotfills/sections/meta-box.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import usePostMetaValue from '@/hooks/use-post-meta-value';
+import Field from '@/interfaces/field';
 
 // Components.
 import FieldRouter from './fieldRouter';
 import Group from './group';
 
 type Props = {
-  field: {
-    title: string,
-    fm: {
-      add_more_label: string,
-      add_more_position: 'bottom' | 'top',
-      attributes: {
-        rows: Number | null,
-      },
-      children: Array<object>,
-      field_class: string,
-      label: string,
-      name: string,
-    }
-  }
+  field: Field,
 };
 
 export default function MetaBox({
