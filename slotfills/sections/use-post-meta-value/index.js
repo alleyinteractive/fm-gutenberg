95082641d94e83e9c35f934ab305437a0783ecf9
Greg Marshall <greg@alley.co>
2022-11-02 09:21:56 -0500

    update docs

--- slotfills/sections/use-post-meta-value/index.js
+++ slotfills/sections/use-post-meta-value/index.js
@@ -25,23 +25,17 @@ const usePostMeta = () => {
 };
 
 /**
- * A custom React hook that wraps useEntityProp for working with a specific
+ * A custom React hook that wraps the window wp object for working with a specific
  * postmeta value. It returns the value for the specified meta key as well as a
  * setter for the meta value. This hook is intended to reduce boilerplate code
  * in components that need to read and write postmeta. It differs from
  * usePostMeta in that it operates on a specific meta key/value pair.
- * By default, it operates on postmeta for the current post, but you can
- * optionally pass a post type and post ID in order to get and set post meta
- * for an arbitrary post.
+ * By default, it operates on postmeta for the current post.
  * @param {string} metaKey - The meta key for which to manage the value.
- * @param {string} postType - Optional. The post type to get and set meta for.
- *                            Defaults to the post type of the current post.
- * @param {number} postId - Optional. The post ID to get and set meta for.
- *                          Defaults to the ID of the current post.
  * @returns {array} An array containing the postmeta value and an update function.
  */
-const usePostMetaValue = (metaKey, postType = null, postId = null) => {
-  const [meta, setMeta] = usePostMeta(postType, postId);
+const usePostMetaValue = (metaKey) => {
+  const [meta, setMeta] = usePostMeta();
 
   /**
    * A helper function for setting the value for the meta key that this hook is
