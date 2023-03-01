4356f45de5943bad9d5bf2391c013c508666fa5f
Greg Marshall <greg@alley.co>
2023-03-01 11:57:01 -0600

    color picker works

--- slotfills/sections/color-picker-field.tsx
+++ slotfills/sections/color-picker-field.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { PanelRow, ColorPicker } from '@wordpress/components';
+import React, { useState } from 'react';
+import { PanelRow, ColorPicker, Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import Field from '@/interfaces/field';
+
+import './color-picker.scss';
 
 interface ColorPickerFieldProps {
   field: Field,
@@ -17,23 +20,49 @@ export default function ColorPickerField({
   index = null,
   label = '',
 }: ColorPickerFieldProps) {
+  const [showPicker, setShowPicker] = useState(false);
+
   const [value, setValue] = index !== null ? valueHook(index) : valueHook(name);
   const initialvalue = typeof value === 'object' ? value[name] : value;
 
-  const updateValue = (color: string) => {
-    const newValue = typeof value === 'object' ? { [name]: color } : color;
+  const updateValue = (newValue: string) => {
     setValue(newValue);
+    setShowPicker(false);
   };
 
   return (
     <PanelRow>
-      <div style={{ flexDirection: 'column' }}>
+      <div className="fm-gutenberg-flex__column fm-gutenberg-popup-wrap">
         <label htmlFor={`${name}_${index}`}>{label}</label>
         <div id={`${name}_${index}`}>
-          <ColorPicker
-            color={initialvalue}
-            onChange={updateValue}
-          />
+          <Button
+            id={`${name}_${index}`}
+            className="fm-gutenberg-color-preview"
+            onClick={() => setShowPicker(!showPicker)}
+            // @ts-expect-error
+            variant="secondary"
+          >
+            <span
+              style={{ backgroundColor: value }}
+            />
+            {__('Select Color', 'fm-gutenberg')}
+          </Button>
+          {showPicker ? (
+            <div className="fm-gutenberg-popup">
+              <Button
+                aria-label={__('Close', 'fm-gutenberg')}
+                onClick={() => setShowPicker(false)}
+                className="fm-gutenberg-popup__close"
+              >
+                x
+              </Button>
+              <ColorPicker
+                color={initialvalue}
+                // @ts-expect-error see https://github.com/DefinitelyTyped/DefinitelyTyped/issues/64541
+                onChange={updateValue}
+              />
+            </div>
+          ) : null}
         </div>
       </div>
     </PanelRow>
